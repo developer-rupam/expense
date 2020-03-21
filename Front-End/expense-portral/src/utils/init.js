e04f351c619ed97a3ecfd34e43cbbaeedddaf0b8
rupamjana <rupam.jana@codomotive.com>
2020-03-21 12:16:29 +0530

    setting up Constatnts in int js

--- Front-End/expense-portral/src/utils/init.js
+++ Front-End/expense-portral/src/utils/init.js
@@ -1,2 +1,18 @@
+const DEVELOPMENTOBJ = {url : 'http://localhost:3000',api : ''};
+const PRODUCTIONOBJ = {url : '',api : ''};
+
+export const mode = "dev" //dev : development; prod : production
+
+if(mode == 'dev'){
+    var url = DEVELOPMENTOBJ.url;
+    var api = DEVELOPMENTOBJ.api;
+}else{
+    var url = PRODUCTIONOBJ.url;
+    var api = PRODUCTIONOBJ.api;
+}
+
+export const ADDR = url;
+export const WEBSERVICE = api;
+
 export const SITENAME = 'EXPENSE';
 export const SITENAMEALIAS = 'expense_portral';
