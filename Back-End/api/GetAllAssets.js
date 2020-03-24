17d210a01df3d58edf79c4724320717bb2f30c2c
rupamjana <rupam.jana@codomotive.com>
2020-03-24 14:05:58 +0530

    get all assets modified

--- Back-End/api/GetAllAssets.js
+++ Back-End/api/GetAllAssets.js
@@ -17,20 +17,26 @@ let response={error:{},result:{}};
 /*** login using post ***/
 router.post('/',(req,res)=>{
    
-
-        db.getDB().collection(collection).find({}).toArray((err,document)=>{
-            if(err){
-                response.error.error_data=1;
-                response.error.error_msg=err;
+        const userId = req.body.userId;
+        if(userId!=undefined ){
+            db.getDB().collection(collection).find({userId : userId}).toArray((err,document)=>{
+                if(err){
+                    response.error.error_data=1;
+                    response.error.error_msg=err;
+                    library.resultData(response,res);
+                }else{
+                    //res.json({result : result,document : result.ops[0]}); 
+                response.error.error_data=0;
+                response.error.error_msg='';
+                    response.result=document;
                 library.resultData(response,res);
-            }else{
-                //res.json({result : result,document : result.ops[0]}); 
-               response.error.error_data=0;
-               response.error.error_msg='';
-                response.result=document;
-               library.resultData(response,res);
-            }
-        });
+                }
+            });
+        }else{
+            response.error.error_data=1;
+            response.error.error_msg='All fields must be field with values';
+            library.resultData(response,res);
+        }
     
     
 });
